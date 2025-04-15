f9299ad9235cc78521e3923828fc03de8bfb85ec
sandy <sandra.de.la.sancha.aguilar.isc.2000@gmail.com>
2025-04-15 13:02:30 -0600

    Se modifica el readme y las categorias

--- JS/main.js
+++ JS/main.js
@@ -3,8 +3,10 @@ const URLMain = "https://fakestoreapi.com/products/";
 const cards = document.getElementById("cards");
 const ulMenu = document.getElementById("ulMenu");
 
-function getData() {
-    fetch(URLMain)
+
+function getData(cat) {
+  const options = {"method" : "GET"};
+    fetch(URLMain+cat, options)
         .then((response) => {
             console.log(response);
             response.json().then((res) => {
@@ -24,22 +26,48 @@ function getData() {
 
 }//getData
 
-getData();
+function getCategories() {
+  const options = {"method" : "GET"};
+    fetch(URLMain+"categories/", options)
+        .then((response) => {
+            //console.log(response);
+            response.json().then((res) => {
+                //console.log("categories:", res);
+                res.forEach((cat)=>{
+                  ulMenu.insertAdjacentHTML("afterbegin",
+                  `<li><a class="dropdown-item" style="cursor:pointer;" onclick="getData('category/${(cat.replace("'","%27"))}');">${cat}</a></li>`);
+                });//forEach
+
+            });
+        })
+        .catch((err) => {
+            main.insertAdjacentHTML("beforeend",
+                `<div class="alert alert-danger" role="alert">
+                        ${err.message}
+               </div>`);
+        });
+
+}//getCategories
+
+getCategories();
+getData("");
 
 function createCards(products) {
     let card = "";
     cards.innerText = "";
 
  products.forEach(product => {
+  
         card += `
 <div class="card mb-3" style="max-width: 540px;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="${product.image}" class="img-fluid rounded-start" alt="${product.title}">
+      <img src="${product.image}" class="img-fluid rounded-start" alt="">
     </div>
     <div class="col-md-8">
       <div class="card-body">
-        <h5 class="card-title">${product.category}</h5>
+         <h3 class="card-title">${product.title}</h3>
+        <h6 class="card-title">${product.category}</h6>
         <p class="card-text">${product.description}</p>
         <p class="card-text"><small class="text-body-secondary">${product.price}</small></p>
       </div>
@@ -48,16 +76,11 @@ function createCards(products) {
 </div>`
 
     })
-
     
+    cards.innerHTML="";
     cards.insertAdjacentHTML("beforeend", card)
 
-    .catch((err) => {
-        main.insertAdjacentHTML("beforeend",
-            `<div class="alert alert-danger" role="alert">
-                    ${err.message}
-           </div>`);
-    });
+    
 
 } 
 
